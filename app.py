9af00b112ca2c16b6a0dfe832fde53d5993612e4
Sarthak <sarthakbhattarai121@gmail.com>
2026-04-09 13:22:44 +0900

    done

--- app.py
+++ app.py
@@ -598,5 +598,4 @@ def delete_user(id):
 # RUN
 # =========================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
